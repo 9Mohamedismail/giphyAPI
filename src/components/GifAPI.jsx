ffb21234d31917420d1bcdadafab3da7123bc5a2
9Mohamedismail <87681402+9Mohamedismail@users.noreply.github.com>
2023-06-19 18:12:33 -0400

    Added GifAPI and GifCard. Website is compelte

--- src/components/GifAPI.jsx
+++ src/components/GifAPI.jsx
@@ -1,13 +1,71 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
 import GifCard from "./GifCard";
+import axios from "axios";
 
 // will render the API information, and send that down to GifCard for presentation
-function GifAPI() {
-    return (  
-        <div>
-            <GifCard></GifCard>
-        </div>
-    );
+function GifAPI(props) {
+  const apiKey = "mNiYePEecf0cV43MboctAfXSTlJ2PsS2";
+  const { search, random, trending } = props;
+
+  const [gifSearchResults, setGifSearchResults] = useState([]);
+  const [gifTrendingResults, setGifTrendingResults] = useState([]);
+  const [gifRandomResults, setGifRandomResults] = useState([]);
+
+  console.log(search);
+
+  useEffect(() => {
+    async function fetchGifsTrending() {
+      try {
+        const result = await axios.get(
+          `https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}`
+        );
+        setGifTrendingResults(result.data.data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchGifsTrending();
+  }, []);
+
+  useEffect(() => {
+    async function fetchGifsSearch() {
+      try {
+        const result = await axios.get(
+          `https://api.giphy.com/v1/gifs/search?q=${search}&api_key=${apiKey}`
+        );
+        setGifSearchResults(result.data.data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchGifsSearch();
+  }, [search]);
+
+  useEffect(() => {
+    async function fetchGifsRandom() {
+      if (random)
+        try {
+          const result = await axios.get(
+            `https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`
+          );
+          setGifRandomResults([result.data.data]);
+        } catch (error) {
+          console.error(error);
+        }
+    }
+    fetchGifsRandom();
+  }, [random]);
+
+  return (
+    <div>
+      <GifCard
+        searchGifs={gifSearchResults}
+        trendingGifs={gifTrendingResults}
+        randomGif={gifRandomResults}
+        displayTrend={trending}
+      />
+    </div>
+  );
 }
 
 export default GifAPI;
--- src/components/GifCard.jsx
+++ src/components/GifCard.jsx
@@ -1,13 +1,50 @@
 import React from "react";
-import GifAPI from "./GifAPI";
+import "../GifCard.css";
 
 // format API information
-function GifCard() {
-    return (  
+function GifCard(props) {
+  const { searchGifs, trendingGifs, randomGif, displayTrend } = props;
+
+  const showTrending = () => {
+    return trendingGifs.map((gif) => (
+      <img key={gif.id} src={gif.images.original.url} alt="trendingGif" />
+    ));
+  };
+
+  return (
+    <div id="trending">
+      {displayTrend && (
         <div>
-           
+          <h1 id="trending">Trending Gifs:</h1>
+          {showTrending()}
         </div>
-    );
+      )}
+
+      <div id="search">
+        {randomGif && (
+          <div>
+            {searchGifs.map((gif) => {
+              return (
+                <img
+                  key={gif.id}
+                  src={gif.images.original.url}
+                  alt="searchGif"
+                />
+              );
+            })}
+          </div>
+        )}
+      </div>
+
+      <div id="random">
+        {randomGif.map((gif) => {
+          return (
+            <img key={gif.id} src={gif.images.original.url} alt="randomGif" />
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default GifCard;
